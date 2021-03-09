976f922e2c734c85506961097d27a5347e99aee7
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-09 15:15:52 +0200

    small fix

--- js/components/laimonas/renderLaimonas.js
+++ js/components/laimonas/renderLaimonas.js
@@ -23,7 +23,7 @@ function renderLaimonas(selector, data) {
     for (let i = 0; i < count; i++) {
         const veikla = laimonoData.data[i];
 
-        
+
         HTML += `<div class="col-4 col-md-6 col-xs-12">
         <div class="services-item text-center">
             <div>
@@ -45,18 +45,19 @@ let timeGeneral = new Date();
 const weekday = timeGeneral.getDay();
 const month = timeGeneral.getMonth();
 const dayOfMonth = timeGeneral.getDate();
-const hour  = timeGeneral.getHours();
+const hour = timeGeneral.getHours();
+
 function dayOfWeek() {
-    if((weekday === 0 || weekday === 6) && ((10 <= hour && hour <= 20))) {
+    if ((weekday === 0 || weekday === 6) && ((10 <= hour && hour <= 20))) {
         return "Savaitgalį namų darbai nuo ryto iki vakaro";
     }
-    if((weekday === 1 || weekday === 2 || weekday === 3 || weekday === 4) && (14 <= hour && hour <= 17)) {
-        return "Dabar man paskaitos nuo 14 iki 17 val.";
+    if ((weekday === 1 || weekday === 2 || weekday === 3 || weekday === 4) && (14 <= hour && hour <= 17)) {
+        return "Dabar man paskaitos nuo 10 iki 13 val.";
     }
-    if((weekday === 1 || weekday === 2 || weekday === 3 || weekday === 4) && ((10 <= hour && hour <= 13) || (22 <= hour || hour <= 1))) {
+    if ((weekday === 1 || weekday === 2 || weekday === 3 || weekday === 4) && ((10 <= hour && hour <= 13) || (22 <= hour || hour <= 1))) {
         return "Aš dabar ruošiu namų darbus";
     }
-    if((weekday === 5) && (10 <= hour <= 13)) {
+    if ((weekday === 5) && (10 <= hour <= 13)) {
         return 'Šiandien "Penktadienio pranešimas"';
     } else {
         return "Dabar man poilsis nuo mokslų."
@@ -64,14 +65,15 @@ function dayOfWeek() {
 }
 dayOfWeek();
 const day = dayOfWeek();
+
 function monthOfYear() {
-    if(month === 0) {
-        return "Java modulis";
-    } 
-    if(month === 1 && dayOfMonth <= 4) {
+    if (month === 0) {
         return "Java modulis";
     }
-    if((month === 1 && dayOfMonth >= 8) || (month === 2) || (month === 3) || (month === 4)) {
+    if (month === 1 && dayOfMonth <= 4) {
+        return "Java modulis";
+    }
+    if ((month === 1 && dayOfMonth >= 8) || (month === 2) || (month === 3) || (month === 4)) {
         return "PHP modulis";
     } else {
         return "mokomės savarankiškai :)"
@@ -79,6 +81,7 @@ function monthOfYear() {
 }
 monthOfYear();
 const month1 = monthOfYear();
+
 function nukeltosKedes() {
     if (weekday === 1 || weekday === 3 || weekday === 5) {
         return "BENAS";
@@ -92,6 +95,7 @@ function nukeltosKedes() {
 }
 nukeltosKedes();
 const kedes = nukeltosKedes();
+
 function indaplove() {
     if (weekday === 1 || weekday === 3 || weekday === 5) {
         return "LUKAS";
@@ -102,6 +106,7 @@ function indaplove() {
 }
 indaplove();
 const indai = indaplove();
+
 function konteineriai() {
     if (((month === 0) && ((dayOfMonth === 14) || (dayOfMonth === 28))) || ((month === 1) && ((dayOfMonth === 11) || (dayOfMonth === 25))) || ((month === 2) && ((dayOfMonth === 11) || (dayOfMonth === 25))) || ((month === 3) && ((dayOfMonth === 8) || (dayOfMonth === 22))) || ((month === 4) && ((dayOfMonth === 6) || (dayOfMonth === 20))) || ((month === 5) && ((dayOfMonth === 3) || (dayOfMonth === 17))) || ((month === 6) && ((dayOfMonth === 1) || (dayOfMonth === 15) || (dayOfMonth === 29))) || ((month === 7) && ((dayOfMonth === 12) || (dayOfMonth === 26))) || ((month === 8) && ((dayOfMonth === 9) || (dayOfMonth === 23))) || ((month === 9) && ((dayOfMonth === 7) || (dayOfMonth === 21))) || ((month === 10) && ((dayOfMonth === 4) || (dayOfMonth === 18))) || ((month === 11) && ((dayOfMonth === 2) || (dayOfMonth === 16) || (dayOfMonth === 30)))) {
         return "Šiandien REIKIA IŠSTUMTI konteinerius. "
